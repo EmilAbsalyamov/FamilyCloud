56785037a4746705ee859fc7b588b43e67557968
Emil Absalyamov <absalyamov.work@gmail.com>
2022-06-30 23:02:22 +0300

    Create root react component & index.html

---
+++ app/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { render } from "react-dom";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <div>App</div>
+    </>
+  );
+};
+
+render(<App />, document.getElementById("root"));
